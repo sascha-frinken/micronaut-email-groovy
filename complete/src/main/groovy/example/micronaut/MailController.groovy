52089fd71a0eec6693575eb846824ea013d90771
Iván López <lopezi@objectcomputing.com>
2019-11-13 18:22:55 +0100

    Use `@MicronautTest` and fix some minor things

--- complete/src/main/groovy/example/micronaut/MailController.groovy
+++ complete/src/main/groovy/example/micronaut/MailController.groovy
@@ -6,26 +6,24 @@ import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
-import io.micronaut.validation.Validated
+
 import javax.validation.Valid
 
 @Slf4j
 @CompileStatic
 @Controller('/mail') // <1>
-@Validated // <2>
 class MailController {
 
     EmailService emailService
 
-    MailController( EmailService  emailService) { // <3>
+    MailController(EmailService  emailService) { // <2>
         this.emailService = emailService
     }
 
-
-    @Post('/send') // <4>
-    HttpResponse send(@Body @Valid EmailCmd cmd) { // <5>
+    @Post('/send') // <3>
+    HttpResponse send(@Body @Valid EmailCmd cmd) { // <4>
         log.info '{}', cmd.toString()
         emailService.send(cmd)
-        HttpResponse.ok()  // <6>
+        HttpResponse.ok()  // <5>
     }
 }
